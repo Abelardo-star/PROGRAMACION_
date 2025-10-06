e5ff4fbfdfef724ed9c0be5b875fa92989fa1c72
Abelardo-star <dracomantw@gmail.com>
2025-10-06 12:29:59 +0200

    cambio ejercicio3

---
+++ practica2/src/ejercicio3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ejercicio3 {
+
+    //Crea un array que contenga 5 números. Realiza un programa que te muestre por pantalla SOLO el más pequeño de ellos.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[5];
+
+        // Pedir los 5 números al usuario
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Introduce el número " + (i + 1) + ": ");
+            numeros[i] = sc.nextInt();
+        }
+        // Suponemos que el primero es el más pequeño
+        int menor = numeros[0];
+
+
+    }
+}
+
+
+
+
