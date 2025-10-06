b9d70ceb6024616dc01e5ec162ffacf4d1988d6d
Abelardo-star <dracomantw@gmail.com>
2025-10-06 11:42:45 +0200

    cambios

--- practica2/src/ejercicio1.java
+++ practica2/src/ejercicio1.java
@@ -18,6 +18,12 @@ public class ejercicio1 {
         //Muestra por consola el indice y el valor
         //indice -> valor
 
+        System.out.println("Contenido del array:");
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.println("Índice " + i + " -> Valor: " + arrayInt[i]);
+        }
+
+
 
     }
 }
