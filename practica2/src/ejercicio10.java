699038fd3403cf28a45a1b0a246a872de24193bb
Abelardo-star <dracomantw@gmail.com>
2025-10-17 08:45:39 +0200

    ejercici10 terminado

--- practica2/src/ejercicio10.java
+++ practica2/src/ejercicio10.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 public class ejercicio10 {
     public static void main(String[] args) {
-        String[] nombres = {"Alice", "Bob", "Charlie"};
-        double[] notas = {90.5, 85.0, 78.5};
+        String[] nombres = {"Alice", "Bob", "Charlie"}; //Lista de los nombres
+        double[] notas = {90.5, 85.0, 78.5}; // Lista de las notas que corresponden a cada uno
 
         Scanner scanner = new Scanner(System.in);
          System.out.println(" Introduce el nombre del estudiante: ");
-         String nombreBuscado = scanner.nextLine();
+         String nombreBuscado = scanner.nextLine(); //Leer el nombre del estudiante escrito por teclado
 
          boolean encontrado = false;
 
+        // Buscar en el array de nombres
          for(int i = 0; i < nombres.length; i++){
              if(nombres[i].equalsIgnoreCase(nombreBuscado)){
                  System.out.println("La nota de " + nombres[i] + " es " + notas[i]);
                  encontrado = true;
-                 break;
+                 break; // Salir del bucle si se encuentra
              }
          }
 
