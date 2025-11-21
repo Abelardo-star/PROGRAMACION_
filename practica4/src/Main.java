2849b88089f837ffcbbef15d2332d7ed0946f6cb
Abelardo-star <dracomantw@gmail.com>
2025-11-21 10:51:01 +0100

    practica4

---
+++ practica4/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.SyncFailedException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+
+        ArrayList<Hospital> hospitals= new ArrayList<Hospital>();
+        //PRECARGA DE DATOS
+
+        //HOSPITALES
+        Hospital h1 = new Hospital("Hospital Central","H001");
+        Hospital h2 = new Hospital("Hospital del Sur","H002");
+
+        //AREAS
+        Areas a1 = new Areas("Cardiologia","CAR1",2, h1);
+        Areas a2 = new Areas("Pediatria","CAR2", 1, h1);
+        Areas a3 = new Areas("Urgencias","CAR3", 0, h2);
+
+        h1.agregarAreas(a1);
+        h1.agregarAreas(a2);
+        h2.agregarAreas(a3);
+
+        Medico m1= new Medico("Jose", a1,"20502084E",18,"Hombre",3000.12, 25-02-2000);
+        a1.aumentarMedicos();
+
+        System.out.println(a1.getNumMedicos());
+
+        Areas a = h1.getAreas().get(0);
+        System.out.println(a.getNombre());
+
+
+
+    }
+}
