0123930abf89a64be82f4068d0c751919f9ab2d6
Abelardo-star <dracomantw@gmail.com>
2026-01-14 13:16:10 +0100

    Stats  comentado

--- Practica5/src/com/juego/modelo/Stats.java
+++ Practica5/src/com/juego/modelo/Stats.java
@@ -1,16 +1,28 @@
 package com.juego.modelo;
 
+// Clase que representa las estadísticas base de un personaje
 public class Stats {
 
-    private int f,i,d,vidaMax;
+    // Atributos del personaje: fuerza, inteligencia, destreza y vida máxima
+    private int f, i, d, vidaMax;
 
-    public Stats(int f,int i,int d,int v){
-        this.f=f; this.i=i; this.d=d; vidaMax=v;
+    // Constructor: inicializa las estadísticas base
+    public Stats(int f, int i, int d, int v){
+        this.f = f;       // fuerza base
+        this.i = i;       // inteligencia base
+        this.d = d;       // destreza base
+        vidaMax = v;      // vida máxima base
     }
 
-    public void bonus(int f,int i,int d){
-        this.f+=f; this.i+=i; this.d+=d;
+    // Aplica un bonus a las estadísticas (por ejemplo, de la clase del personaje)
+    public void bonus(int f, int i, int d){
+        this.f += f;      // suma bonus a la fuerza
+        this.i += i;      // suma bonus a la inteligencia
+        this.d += d;      // suma bonus a la destreza
     }
 
-    public int getVidaMax(){return vidaMax;}
+    // Devuelve la vida máxima del personaje
+    public int getVidaMax(){
+        return vidaMax;
+    }
 }
