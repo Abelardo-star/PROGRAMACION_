d705cab07a004fd889ca7134b430e9fe8eb91b08
Abelardo-star <dracomantw@gmail.com>
2026-01-14 09:37:29 +0100

    Stats terminado

--- Practica5/src/com/juego/modelo/Stats.java
+++ Practica5/src/com/juego/modelo/Stats.java
@@ -1,13 +1,16 @@
 package com.juego.modelo;
 
 public class Stats {
-    public int f,i,d,vidaMax;
+
+    private int f,i,d,vidaMax;
 
     public Stats(int f,int i,int d,int v){
         this.f=f; this.i=i; this.d=d; vidaMax=v;
     }
 
-    public void sumar(int f,int i,int d){
+    public void bonus(int f,int i,int d){
         this.f+=f; this.i+=i; this.d+=d;
     }
+
+    public int getVidaMax(){return vidaMax;}
 }
