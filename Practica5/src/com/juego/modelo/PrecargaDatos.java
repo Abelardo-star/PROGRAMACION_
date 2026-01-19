7514efb289443bd7294b26a9a161fb0f67c6f687
Abelardo-star <dracomantw@gmail.com>
2026-01-19 13:35:23 +0100

    Modelo terminado y comentado(Menos combate)

--- Practica5/src/com/juego/modelo/PrecargaDatos.java
+++ Practica5/src/com/juego/modelo/PrecargaDatos.java
@@ -3,20 +3,76 @@ package com.juego.modelo;
 import com.juego.razas.*;
 import com.juego.clases.*;
 
-// Clase que precarga personajes de ejemplo en el juego
+// Clase para crear datos precargados de razas, clases y personajes
 public class PrecargaDatos {
 
-    // Metodo estático que recibe un gestor de personajes y agrega personajes predeterminados
-    public static void cargar(GestorPersonajes g){
+    // Carga inicial de personajes al juego
+    public static void cargar(GestorPersonajes g) {
 
-        // Se crean y agregan personajes con nombre, raza y clase
-        g.add(new Personaje("Arthas", new Humano(), new Guerrero()));
-        g.add(new Personaje("Uther", new Enano(), new Paladin()));
-        g.add(new Personaje("Li", new Humano(), new Monje()));
-        g.add(new Personaje("Shade", new Elfo(), new Picaro()));
-        g.add(new Personaje("Oak", new Elfo(), new Druida()));
-        g.add(new Personaje("Faith", new Humano(), new Sacerdote()));
-        g.add(new Personaje("Merlin", new Elfo(), new Mago()));
-        g.add(new Personaje("Lyra", new Humano(), new Bardo()));
+        g.add(new Personaje("Lulu", crearHumano(), crearGuerrero()));
+        g.add(new Personaje("Vargan", crearEnano(), crearPaladin()));
+        g.add(new Personaje("Merlin", crearElfo(), crearMago()));
+    }
+
+    // Métodos para crear razas con atributos iniciales
+    public static Raza crearHumano() {
+        return new Humano("Humano", 5, 5, 5, 100);
+    }
+
+    public static Raza crearElfo() {
+        return new Elfo("Elfo", 4, 6, 7, 90);
+    }
+
+    public static Raza crearEnano() {
+        return new Enano("Enano", 7, 4, 4, 110);
+    }
+
+    // Métodos para crear clases y asignar habilidades
+    public static Clase crearGuerrero() {
+        return new Guerrero("Corte Brutal", 25,
+                            "Grito de Guerra", 5,
+                            "Lanzamiento de hacha", 10);
+    }
+
+    public static Clase crearPaladin() {
+        return new Paladin("Espada Sagrada", 18,
+                           "Luz Divina", 15,
+                           "Juicio Celestial", 10);
+    }
+
+    public static Clase crearMonje() {
+        return new Monje("Puño del Dragón", 17,
+                         "Meditación", 8,
+                         "Onda Chi", 18);
+    }
+
+    public static Clase crearPicaro() {
+        return new Picaro("Puñalada", 22,
+                          "Vendaje Rápido", 6,
+                          "Daga Arrojadiza", 20);
+    }
+
+    public static Clase crearDruida() {
+        return new Druida("Garra Salvaje", 10,
+                          "Recrecimiento", 20,
+                          "Raíces Enredadoras", 15);
+    }
+
+    public static Clase crearSacerdote() {
+        return new Sacerdote("Golpe Bendito", 8,
+                             "Sanación Mayor", 25,
+                             "Luz Castigadora", 12);
+    }
+
+    public static Clase crearMago() {
+        return new Mago("Bastón Arcano", 5,
+                        "Escudo Mágico", 5,
+                        "Bola de Fuego", 40);
+    }
+
+    public static Clase crearBardo() {
+        return new Bardo("Golpe Rítmico", 10,
+                         "Canción Curativa", 14,
+                         "Nota Letal", 18);
     }
 }
