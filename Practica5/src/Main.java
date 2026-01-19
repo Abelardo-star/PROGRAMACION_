84ad4224ce350f0059d2b14080e9aa3e3815319d
Abelardo-star <dracomantw@gmail.com>
2026-01-19 13:39:02 +0100

    Combate y Main terminado

--- Practica5/src/Main.java
+++ Practica5/src/Main.java
@@ -1,13 +1,15 @@
-import com.juego.modelo.*;
 import com.juego.presentacion.*;
+import com.juego.modelo.*;
 
 public class Main {
-
     public static void main(String[] args){
 
         GestorPersonajes gestor = new GestorPersonajes();
         PrecargaDatos.cargar(gestor);
 
-        new Presentador(new Vista(),gestor).iniciar();
+        Vista vista = new VistaConsola();
+        Presentador p = new Presentador(vista,gestor);
+
+        p.iniciar();
     }
 }
