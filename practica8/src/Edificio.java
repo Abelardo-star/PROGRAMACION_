1fa3792b4f2d1739453e81a826e8d1fc3046d6d8
Abelardo-star <dracomantw@gmail.com>
2026-02-16 13:58:40 +0100

    Edificio Terminados

--- practica8/src/Edificio.java
+++ practica8/src/Edificio.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Edificio {
 
+    private String nombre;
+    private String direccion;
+
+    private List<Maquinaria> maquinarias;
+    private List<Plantas> plantas;
+
+    public Edificio(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.maquinarias = new ArrayList<>();
+        this.plantas = new ArrayList<>();
+    }
+
+    public void agregarMaquinaria(Maquinaria m) {
+        maquinarias.add(m);
+    }
+
+    public void agregarPlanta(Plantas p) {
+        plantas.add(p);
+    }
 }
