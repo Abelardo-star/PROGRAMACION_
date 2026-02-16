8adcc2f2b2185416abecfa6adeeeca886b63138d
Abelardo-star <dracomantw@gmail.com>
2026-02-16 13:33:21 +0100

    Pasillos, Espacios, Comunes y SalaServidores Creados

---
+++ practica8/src/Comunes.java
@@ -0,0 +1,9 @@
+public class Comunes extends Espacios {
+
+    protected String nombre;
+
+    public Comunes(double tamano, String nombre) {
+        super(tamano);
+        this.nombre = nombre;
+    }
+}
