f9722682b7b3d14ea563559914923978ba69c790
Abelardo-star <dracomantw@gmail.com>
2026-02-25 13:49:00 +0100

    La primera parte del Helper para los items hecha

--- practica7/src/com/rpg/utils/JsonHelper.java
+++ practica7/src/com/rpg/utils/JsonHelper.java
@@ -1,20 +1,54 @@
 package com.rpg.utils;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.rpg.model.cuidades;
+import com.rpg.model.item;
 
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JsonHelper {
 
-    public JsonHelper() {
-    }
+    public JsonHelper(){}
+    public static List<item> leerItem() {
 
-    private String reader;
+        List<item> listaItem = new ArrayList<>();
 
-    List<cuidades> lista = new Gson().fromJson(reader, new TypeToken<List<cuidades>>() {}.getType());
+        try {
+            FileReader reader = new FileReader("practica7/ficheros/item.json");
+            Gson g = new Gson();
+            List<item> lista = g.fromJson(reader, new TypeToken<List<item>>() {
+            }.getType());
 
-    public static List<cuidades> readListcuidades(String path) {
-
+            for (item t : lista) {
+                System.out.println(t.getNombre());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return listaItem;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
