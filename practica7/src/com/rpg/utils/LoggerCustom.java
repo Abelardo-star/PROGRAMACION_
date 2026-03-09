9fbf292950e330a4e172ac045d6de989a72d2bee
Abelardo-star <dracomantw@gmail.com>
2026-03-09 14:05:21 +0100

    LoggerCustom terminado y creado el fichero del log

--- practica7/src/com/rpg/utils/LoggerCustom.java
+++ practica7/src/com/rpg/utils/LoggerCustom.java
@@ -1,4 +1,30 @@
 package com.rpg.utils;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class LoggerCustom {
+    private static String Log = "practica7/ficheros/info.log";
+    public static void Error(String mensaje){
+
+        try{
+            FileWriter fw = new FileWriter(Log, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            LocalDateTime tiempo = LocalDateTime.now();
+            DateTimeFormatter formateo = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String fecha = tiempo.format(formateo);
+
+            String linea = "{" + fecha + "}" + mensaje;
+
+            bw.write(linea);
+            bw.newLine();
+            bw.close();
+
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el log");
+        }
+    }
 }
