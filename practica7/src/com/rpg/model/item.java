7789be2abdd4f9f3ac7e0af7f3669d8ac971c435
Abelardo-star <dracomantw@gmail.com>
2026-03-10 22:18:33 +0100

    Creado un array para el equipo por si no existe el objeto

--- practica7/src/com/rpg/model/item.java
+++ practica7/src/com/rpg/model/item.java
@@ -1,10 +1,14 @@
 package com.rpg.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class item {
     private String id;
     private String nombre;
     private String tipo;
     private Integer valor;
+    private List<item> equipo;
 
     public item(String id , String nombre , String tipo ,Integer valor){
         this.id=id;
@@ -29,6 +33,14 @@ public class item {
     public String getTipo() {
         return tipo;
     }
+
+    //por si el objeto no existe
+    public List<item> getEquipo(){
+        if (equipo == null) {
+            equipo = new ArrayList<>();
+        }
+        return equipo;
+    }
     //SET
 
     public void setNombre(String nombre) {
@@ -46,4 +58,9 @@ public class item {
     public void setValor(Integer valor) {
         this.valor = valor;
     }
+
+    public void setEquipo(List<item> equipo) {
+        this.equipo = equipo;
+    }
+
 }
